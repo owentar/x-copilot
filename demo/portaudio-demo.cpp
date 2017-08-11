225f617610799111e72468630dbc5ced9f6281a0
Hernan Carrizo <owentar@gmail.com>
2017-08-11 19:12:41 +0100

    update demo to print out the phrase recognized

--- demo/portaudio-demo.cpp
+++ demo/portaudio-demo.cpp
@@ -2,11 +2,14 @@
 #include "Recognizer.h"
 #include "Microphone.h"
 
+#include <iostream>
+
 int main(int argc, char *argv[]) {
     Microphone mic;
     Pocketsphinx pocketsphinx;
     Recognizer recognizer(&pocketsphinx, &mic);
 
+    recognizer.connect([](const std::string phrase) { std::cout << phrase << std::endl; } );
     recognizer.start();
 
     while(true) { }
--- src/PocketsphinxWrapper.cpp
+++ src/PocketsphinxWrapper.cpp
@@ -7,8 +7,8 @@ void Pocketsphinx::init()
 {
     cmd_ln_t* config = cmd_ln_init(NULL, ps_args(), TRUE,       // Load the configuration structure - ps_args() passes the default values
     "-hmm", "/usr/local/share/pocketsphinx/model/en-us/en-us",  // path to the standard english language model
-    "-lm", "/home/owentar/Projects/X-Plane/x-copilot/xcopilot/pocketsphinx-data/xp-XP/language-model.lm",
-    "-dict", "/home/owentar/Projects/X-Plane/x-copilot/xcopilot/pocketsphinx-data/xp-XP/pronounciation-dictionary.dic",
+    "-lm", "/home/owentar/Projects/X-Plane/x-copilot-py/xcopilot/pocketsphinx-data/xp-XP/language-model.lm",
+    "-dict", "/home/owentar/Projects/X-Plane/x-copilot-py/xcopilot/pocketsphinx-data/xp-XP/pronounciation-dictionary.dic",
     "-logfn", "/dev/null",                                      // suppress log info from being sent to screen
      NULL);
     ps = ps_init(config);
