4fde1b823ba33fe47e52524c032ef6b1898f6034
Hernan Carrizo <owentar@gmail.com>
2017-11-19 23:04:50 +0000

    add XPlane SDK interface to wrap x-plane SDK calls

---
+++ demo/XPlaneDataRefSDKStub.h
@@ -0,0 +1,14 @@
+#ifndef X_COPILOT_XPLANEDATAREFSDKSTUB_H
+#define X_COPILOT_XPLANEDATAREFSDKSTUB_H
+
+#include "XPlaneDataRefSDK.h"
+
+class XPlaneDataRefSDKStub : public XPlaneDataRefSDK {
+public:
+    virtual XPLMDataRef findDataRef(const std::string& name) { return nullptr; }
+    virtual int getIntValue(XPLMDataRef id) const { return 0; }
+    virtual void setValue(XPLMDataRef id, const int value) const {}
+};
+
+
+#endif //X_COPILOT_XPLANEDATAREFSDKSTUB_H
---
+++ src/XPlaneDataRefSDK.h
@@ -0,0 +1,15 @@
+#ifndef X_COPILOT_XPLANESDK_H
+#define X_COPILOT_XPLANESDK_H
+
+#include <string>
+
+#include "XPLMDataAccess.h"
+
+class XPlaneDataRefSDK {
+public:
+    virtual XPLMDataRef findDataRef(const std::string& name) { return nullptr; };
+    virtual int getIntValue(XPLMDataRef id) const { return XPLMGetDatai(id); };
+    virtual void setValue(XPLMDataRef id, const int value) const { XPLMSetDatai(id, value); };
+};
+
+#endif //X_COPILOT_XPLANESDK_H
