8182ce691064b4bb0e31ba79b36d60a7bb52e93c
Hernan Carrizo <owentar@gmail.com>
2018-01-07 22:59:30 +0000

    add MicrophoneHandler to xcopilot namespace

--- src/MicrophoneHandler.h
+++ src/MicrophoneHandler.h
@@ -1,11 +1,12 @@
 #ifndef MICROPHONEHANDLER_H_INCLUDED
 #define MICROPHONEHANDLER_H_INCLUDED
 
-template<class T>
-class MicrophoneHandler
-{
-public:
-    virtual void handleAudio(const T* rawData, unsigned long frameCount) = 0;
-};
+namespace xcopilot {
+    template<class T>
+    class MicrophoneHandler {
+    public:
+        virtual void handleAudio(const T *rawData, unsigned long frameCount) = 0;
+    };
+}
 
 #endif // MICROPHONEHANDLER_H_INCLUDED
