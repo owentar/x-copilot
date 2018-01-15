fb4904e4b9df4e60ce520d76be39835a1347aece
Hernan Carrizo <owentar@gmail.com>
2018-01-15 08:57:22 +0000

    uppercase expected commands to match what pocketsphinx is returning as phrases (all uppercase)

--- src/utils.cpp
+++ src/utils.cpp
@@ -7,16 +7,16 @@
 #include "boost/algorithm/string.hpp"
 
 std::map<std::string, int> WORD_TO_NUMBER = {
-        { "zero", 0 },
-        { "one", 1 },
-        { "two", 2 },
-        { "three", 3 },
-        { "four", 4 },
-        { "five", 5 },
-        { "six", 6 },
-        { "seven", 7 },
-        { "eight", 8 },
-        { "nine", 9 }
+        { "ZERO", 0 },
+        { "ONE", 1 },
+        { "TWO", 2 },
+        { "THREE", 3 },
+        { "FOUR", 4 },
+        { "FIVE", 5 },
+        { "SIX", 6 },
+        { "SEVEN", 7 },
+        { "EIGHT", 8 },
+        { "NINE", 9 }
 };
 
 std::vector<int> xcopilot::parseNumbers(const std::string& numbersAsWords)
--- test/utilsTest.cpp
+++ test/utilsTest.cpp
@@ -11,23 +11,28 @@ class UtilsTest : public Test
 
 TEST_F(UtilsTest, ParseNumbersWordsToNumbersArray)
 {
-    ASSERT_THAT(parseNumbers("one two"), ElementsAre(1, 2));
-    ASSERT_THAT(parseNumbers("four nine seven zero eight"), ElementsAre(4, 9, 7, 0, 8));
-    ASSERT_THAT(parseNumbers("three five zero six"), ElementsAre(3, 5, 0, 6));
+    ASSERT_THAT(parseNumbers("ONE TWO"), ElementsAre(1, 2));
+    ASSERT_THAT(parseNumbers("FOUR NINE SEVEN ZERO EIGHT"), ElementsAre(4, 9, 7, 0, 8));
+    ASSERT_THAT(parseNumbers("THREE FIVE ZERO SIX"), ElementsAre(3, 5, 0, 6));
 }
 
 TEST_F(UtilsTest, ParseToInteger)
 {
-    ASSERT_THAT(parseToInt("nine five zero zero"), Eq(9500));
-    ASSERT_THAT(parseToInt("four three"), Eq(43));
-    ASSERT_THAT(parseToInt("zero"), Eq(0));
+    ASSERT_THAT(parseToInt("NINE FIVE ZERO ZERO"), Eq(9500));
+    ASSERT_THAT(parseToInt("FOUR THREE"), Eq(43));
+    ASSERT_THAT(parseToInt("ZERO"), Eq(0));
 }
 
 TEST_F(UtilsTest, ParseToDecimal)
 {
-    ASSERT_THAT(parseToDecimal("two nine nine two", 0), DoubleEq(2992));
-    ASSERT_THAT(parseToDecimal("two nine nine two", 1), DoubleEq(299.2));
-    ASSERT_THAT(parseToDecimal("two nine nine two", 2), DoubleEq(29.92));
-    ASSERT_THAT(parseToDecimal("two nine nine two", 3), DoubleEq(2.992));
-    ASSERT_THAT(parseToDecimal("two nine nine two", 4), DoubleEq(0.2992));
+    ASSERT_THAT(parseToDecimal("TWO NINE NINE TWO", 0), DoubleEq(2992));
+    ASSERT_THAT(parseToDecimal("TWO NINE NINE TWO", 1), DoubleEq(299.2));
+    ASSERT_THAT(parseToDecimal("TWO NINE NINE TWO", 2), DoubleEq(29.92));
+    ASSERT_THAT(parseToDecimal("TWO NINE NINE TWO", 3), DoubleEq(2.992));
+    ASSERT_THAT(parseToDecimal("TWO NINE NINE TWO", 4), DoubleEq(0.2992));
+}
+
+TEST_F(UtilsTest, ParseToFloat)
+{
+    ASSERT_THAT(parseToFloat("THREE ZERO ZERO FOUR"), FloatEq(30.04));
 }
