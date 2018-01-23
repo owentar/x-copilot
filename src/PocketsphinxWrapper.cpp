317aa10aaf9554df7b93c9af2478fdde7057a234
Hernan Carrizo <owentar@gmail.com>
2018-01-23 20:18:05 +0000

    refactor PocketsphinxWrapper to make absolute paths windows friednly

--- src/PocketsphinxWrapper.cpp
+++ src/PocketsphinxWrapper.cpp
@@ -6,6 +6,12 @@
 #include "pocketsphinx/pocketsphinx.h"
 #include "Logger.h"
 
+#ifdef IBM
+#define POCKETSPHINX_LOGFN "nul"
+#else
+#define POCKETSPHINX_LOGFN "/dev/null"
+#endif
+
 using namespace xcopilot;
 
 boost::filesystem::path getPocketsphinxDataPathForResource(const std::string& resource)
@@ -16,14 +22,14 @@ boost::filesystem::path getPocketsphinxDataPathForResource(const std::string& re
 void Pocketsphinx::start()
 {
     Logger::getInstance()->debug("Starting pocketsphinx");
-    boost::filesystem::path hmm = getPocketsphinxDataPathForResource("acoustic-model");
-    boost::filesystem::path lm = getPocketsphinxDataPathForResource("language-model.lm");
-    boost::filesystem::path dict = getPocketsphinxDataPathForResource("pronounciation-dictionary.dic");
+    auto hmm = getPocketsphinxDataPathForResource("acoustic-model").generic_string();
+    auto lm = getPocketsphinxDataPathForResource("language-model.lm").generic_string();
+    auto dict = getPocketsphinxDataPathForResource("pronounciation-dictionary.dic").generic_string();
     cmd_ln_t* config = cmd_ln_init(NULL, ps_args(), TRUE,       // Load the configuration structure - ps_args() passes the default values
     "-hmm", hmm.c_str(),  // path to the standard english language model
     "-lm", lm.c_str(),
     "-dict", dict.c_str(),
-    "-logfn", "/dev/null",                                      // suppress log info from being sent to screen
+    "-logfn", POCKETSPHINX_LOGFN,                                      // suppress log info from being sent to screen
     NULL);
     ps = ps_init(config);
 
