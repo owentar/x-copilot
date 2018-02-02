ef9a7eb1a4b2e3c88ea7129be9489427eb61bb01
Hernan Carrizo <owentar@gmail.com>
2018-02-02 21:19:36 +0000

    fix wrong jsgf variable reference

--- src/PocketsphinxWrapper.cpp
+++ src/PocketsphinxWrapper.cpp
@@ -29,7 +29,7 @@ void Pocketsphinx::start()
     cmd_ln_t* config = cmd_ln_init(NULL, ps_args(), TRUE,       // Load the configuration structure - ps_args() passes the default values
     "-hmm", hmm.c_str(),  // path to the standard english language model
 //    "-lm", lm.c_str(),
-    "-jsgf", lm.c_str(),
+    "-jsgf", jsgf.c_str(),
     "-dict", dict.c_str(),
     "-logfn", POCKETSPHINX_LOGFN,                                      // suppress log info from being sent to screen
     NULL);
