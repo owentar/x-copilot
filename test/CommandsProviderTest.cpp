3b138ca982ae586f912da55bd8d4257573c68c6b
Hernan Carrizo <owentar@gmail.com>
2019-03-03 12:24:44 -0300

    refactor test code to make it readable and improve assertions

--- test/CommandsProviderTest.cpp
+++ test/CommandsProviderTest.cpp
@@ -1,5 +1,7 @@
 #include "gmock/gmock.h"
 
+#include <algorithm>
+#include <functional>
 #include <memory>
 
 #include "CommandsProvider.h"
@@ -7,13 +9,23 @@
 using namespace testing;
 using namespace xcopilot;
 
+typedef std::shared_ptr<CommandRecognizer> CommandRecognizerPtr;
+
+auto isCommandEqual(const CommandRecognizerPtr& command) {
+    return std::bind(std::equal_to(), std::placeholders::_1, command);
+};
+
+auto isCommandPresent(const std::vector<CommandRecognizerPtr>& commands, const CommandRecognizerPtr& command) {
+    return std::any_of(commands.begin(), commands.end(), isCommandEqual(command));
+}
+
 class CommandsConfigReaderMock : public CommandsConfigReader {
 public:
     explicit CommandsConfigReaderMock() : CommandsConfigReader() {}
 
     MOCK_METHOD1(getAircraftCommandsMatchers, std::vector<AircraftCommandsMatcher>(const std::string&));
 
-    MOCK_METHOD1(getCommandsFromFile, std::vector<std::shared_ptr<CommandRecognizer>>(const std::string&));
+    MOCK_METHOD1(getCommandsFromFile, std::vector<CommandRecognizerPtr>(const std::string&));
 };
 
 class CommandsProviderTest : public Test {
@@ -21,17 +33,19 @@ public:
     CommandsProviderTest() : commandsConfigReaderMock{std::make_unique<NiceMock<CommandsConfigReaderMock>>()} {};
 protected:
     std::unique_ptr<NiceMock<CommandsConfigReaderMock>> commandsConfigReaderMock;
+    CommandRecognizerPtr defaultCommand1;
+    CommandRecognizerPtr defaultCommand2;
 
     virtual void SetUp() {
         std::vector<AircraftCommandsMatcher> aircraftMatchers = {{"test", "test", "test", "test", "test"}};
         EXPECT_CALL(*commandsConfigReaderMock, getAircraftCommandsMatchers(DEFAULT_INDEX_FILE_PATH)) // TODO: verify how should I import DEFAULT_INDEX_FILE_PATH const
                 .WillOnce(Return(aircraftMatchers));
 
-        auto defaultCommand1 = std::make_shared<CommandRecognizer>(
+        defaultCommand1 = std::make_shared<CommandRecognizer>(
                 CommandMetadata("command-1", CommandType::FLOAT, "command-1", {}));
-        auto defaultCommand2 = std::make_shared<CommandRecognizer>(
+        defaultCommand2 = std::make_shared<CommandRecognizer>(
                 CommandMetadata("command-2", CommandType::FLOAT, "command-2", {}));
-        std::vector<std::shared_ptr<CommandRecognizer>> defaultCommands = {defaultCommand1, defaultCommand2};
+        std::vector<CommandRecognizerPtr> defaultCommands = {defaultCommand1, defaultCommand2};
         EXPECT_CALL(*commandsConfigReaderMock, getCommandsFromFile(DEFAULT_COMMANDS_FILE_PATH))
                 .WillOnce(Return(defaultCommands));
     }
@@ -48,11 +62,15 @@ TEST_F(CommandsProviderTest, MergeCommandsWhenMatchIsFoundOnIndex) {
             CommandMetadata("command-1", CommandType::INT, "command-1", {}));
     auto aircraftCommand2 = std::make_shared<CommandRecognizer>(
             CommandMetadata("command-3", CommandType::INT, "command-3", {}));
-    std::vector<std::shared_ptr<CommandRecognizer>> aircraftCommands = {aircraftCommand1, aircraftCommand2};
+    std::vector<CommandRecognizerPtr> aircraftCommands = {aircraftCommand1, aircraftCommand2};
     EXPECT_CALL(*commandsConfigReaderMock, getCommandsFromFile("test"))
             .WillOnce(Return(aircraftCommands));
 
     CommandsProvider commandsProvider(std::move(commandsConfigReaderMock));
     auto commands = commandsProvider.getCommandsForAircraft("test", "test", "test");
     ASSERT_THAT(commands.size(), Eq(3));
+    ASSERT_THAT(isCommandPresent(commands, aircraftCommand1), Eq(true));
+    ASSERT_THAT(isCommandPresent(commands, aircraftCommand2), Eq(true));
+    ASSERT_THAT(isCommandPresent(commands, defaultCommand2), Eq(true));
+    ASSERT_THAT(isCommandPresent(commands, defaultCommand1), Eq(false));
 }
